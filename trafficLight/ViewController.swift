2029d78d5bab702b360fc89921831ddfb975dad2
Pavel Ausianikau <lensli.p@gmail.com>
2020-11-13 17:46:10 +0300

    click the button

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var buttonStart: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.layer.cornerRadius = 110
+        redView.alpha = 0.1
+        yellowView.layer.cornerRadius = 110
+        yellowView.alpha = 0.1
+        greenView.layer.cornerRadius = 110
+        greenView.alpha = 0.1
+        buttonStart.layer.cornerRadius = 10
+        buttonStart.setTitle("STOP", for: .normal)
     }
-
-
+    
+//    @IBAction func pushTheButton() {
+//        greenView.alpha += 0.1
+//    }
+//    
+    
 }
 
